6ac74955f1b783aebceeb87af3c19292262f42c4
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-02-07 23:03:18 +0100

    Add comment crediting ErlangCentral.org Wiki
    for providing ISO 8601 date format function.

--- src/events.erl
+++ src/events.erl
@@ -11,6 +11,6 @@ state_goal({ Date, Goal, Estimation  }) ->
     ]
   ).
 
-
+% By courtesy of http://www2.erlangcentral.org/wiki/?title=Converting_Between_struct:time_and_ISO8601_Format
 iso_8601_fmt({Year,Month,Day}) ->
   io_lib:format("~4.10.0B-~2.10.0B-~2.10.0B", [Year, Month, Day]).
